9269ff04291665fad0b1d6e24aacbb9b4e94dce3
Mark G <mark@boredfish.com>
2022-06-18 16:10:49 +0000

    formatting and options

--- src/Node/RandomNumberNode.php
+++ src/Node/RandomNumberNode.php
@@ -4,13 +4,13 @@ namespace Flow\Node;
 
 class RandomNumberNode extends AbstractNode
 {
-	public function getOutputs(): array
-	{
-		return ['number' => 'integer'];
-	}
+    public function getOutputs(): array
+    {
+        return ['number' => 'integer'];
+    }
 
-	public function execute(): void
-	{
-		$this->setOutputValue('number', rand(1, 1000));
-	}
+    public function execute(): void
+    {
+        $this->setOutputValue('number', rand(1, 1000));
+    }
 }
--- src/Node/SubtractorNode.php
+++ src/Node/SubtractorNode.php
@@ -4,25 +4,25 @@ namespace Flow\Node;
 
 class SubtractorNode extends AbstractNode
 {
-	public function getInputs(): array
-	{
-		return [
-			'number1' => 'integer',
-			'number2' => 'integer'
-		];
-	}
+    public function getInputs(): array
+    {
+        return [
+            'number1' => 'integer',
+            'number2' => 'integer'
+        ];
+    }
 
-	public function getOutputs(): array
-	{
-		return [
-			'result' => 'integer'
-		];
-	}
+    public function getOutputs(): array
+    {
+        return [
+            'result' => 'integer'
+        ];
+    }
 
-	public function execute(): void
-	{
-		$number1 = $this->getInputValue('number1');
-		$number2 = $this->getInputValue('number2');
-		$this->setOutputValue('result', $number1 - $number2);
-	}
+    public function execute(): void
+    {
+        $number1 = $this->getInputValue('number1');
+        $number2 = $this->getInputValue('number2');
+        $this->setOutputValue('result', $number1 - $number2);
+    }
 }
