789840caba93b680023d0eba4b60e10d96789fae
Mark G <mark@boredfish.com>
2022-06-23 07:34:28 +0000

    math nodetype option; docs

--- src/Node/MathNode.php
+++ src/Node/MathNode.php
@@ -4,6 +4,7 @@ namespace Flow\Node;
 
 use Flow\Base\AbstractNode;
 use Flow\Base\Input;
+use Flow\Base\Option;
 use Flow\Base\Output;
 
 class MathNode extends AbstractNode
@@ -13,12 +14,20 @@ class MathNode extends AbstractNode
         $this->addInput(new Input($this, 'number1', 'integer'));
         $this->addInput(new Input($this, 'number2', 'integer'));
         $this->addOutput(new Output($this, 'result', 'integer'));
+        $this->addOption(new Option($this, 'operator', 'string'))->setValue('+');
     }
 
     public function run(): void
     {
         $in1 = $this->getInput('number1')->getValue();
         $in2 = $this->getInput('number2')->getValue();
-        $this->getOutput('result')->setValue($in1 + $in2);
+        $op = $this->getOption('operator')->getValue();
+
+        $result = null;
+        $expression = sprintf("%d %.1s %d", $in1, $op, $in2);
+        $code = "\$result = {$expression};";
+        eval($code);
+dump($expression);
+        $this->getOutput('result')->setValue($result);
     }
 }
--- src/Runner.php
+++ src/Runner.php
@@ -6,10 +6,22 @@ use Flow\Base\AbstractNode;
 use Flow\Base\FlowException;
 use Throwable;
 
+/**
+ * Main setup/entry point for the Flow
+ */
 class Runner
 {
+    /**
+     * @var AbstractNode[]
+     */
     private array $nodes = [];
 
+    /**
+     * Load from definition file
+     *
+     * @param string $filename
+     * @return Runner self
+     */
     public function load(string $filename): self
     {
         try {
@@ -19,16 +31,23 @@ class Runner
         }
 
         $connections = [];
+        $options = [];
 
-        // main nodes
+        // main nodes & options
         foreach ($definition['nodes'] as $nodeDefinition) {
             $class = '\\Flow\\Node\\' . $nodeDefinition['type'] . 'Node';
-            $this->addNode(new $class($nodeDefinition['id']));
+            $node = $this->addNode(new $class($nodeDefinition['id']));
             if (!empty($nodeDefinition['connections'])) {
                 $connections[$nodeDefinition['id']] = $nodeDefinition['connections'];
             }
+            if (!empty($nodeDefinition['options'])) {
+                foreach ($nodeDefinition['options'] as $optName => $optVal) {
+                    $node->getOption($optName)->setValue($optVal);
+                }
+            }
         }
 
+        // connections
         foreach ($connections as $from => $to) {
             $fromNode = $this->getNode($from);
             foreach ($to as $map) {
@@ -46,11 +65,27 @@ class Runner
         return $this;
     }
 
-    public function addNode(AbstractNode $node): AbstractNode
+    /**
+     * Add a new node to the Flow
+     *
+     * @param AbstractNode $node
+     * @param bool $replace Whether to replace a node of the same name, if already set
+     * @return AbstractNode The added node
+     */
+    public function addNode(AbstractNode $node, bool $replace = false): AbstractNode
     {
+        if (!$replace && !empty($this->nodes[$node->getIdentifier()])) {
+            throw new FlowException('Node with identifer \'' . $node->getIdentifier() . '\' already exists');
+        }
         return $this->nodes[$node->getIdentifier()] = $node;
     }
 
+    /**
+     * Fetch a node by identifier
+     *
+     * @param string $identifier
+     * @return AbstractNode
+     */
     public function getNode(string $identifier): AbstractNode
     {
         if (!isset($this->nodes[$identifier])) {
@@ -59,6 +94,11 @@ class Runner
         return $this->nodes[$identifier];
     }
 
+    /**
+     * Execute the flow. Loops all nodes with no inputs available
+     *
+     * @return void
+     */
     public function run(): void
     {
         foreach ($this->nodes as $node) {
